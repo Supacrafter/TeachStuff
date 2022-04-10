cb71cad64041b2213c0d870a655168b58fcd6e07
Cooper Fleishman <61219283+Supacrafter@users.noreply.github.com>
2022-04-09 23:08:50 -0400

    Add files via upload

---
+++ dataTypes.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Test
+{
+    public void main() 
+    {
+        int foo;
+        foo = 10;
+        foo = "10";
+
+        Console.WriteLine(foo);
+    }
+}
---
+++ dataTypes.py
@@ -0,0 +1,13 @@
+# birthday = 29
+# age = 16
+
+# birthday = birthday + 1  # Adding 1 to birthday
+
+# print(birthday)
+
+
+foo = None  # Declaring Foo as null
+foo = 10  # foo now equals 10
+foo = "10"
+
+print(type(foo))
